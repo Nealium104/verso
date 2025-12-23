23bc92c42a9ef4f486e0a136242aaa15b2b7edd6
Neal <neal.powers@outlook.com>
2025-12-23 15:06:00 -0500

    add create, read, update, delete for db

--- src/repositories/PostgresCardRepository.ts
+++ src/repositories/PostgresCardRepository.ts
@@ -18,4 +18,70 @@ export class PostgresCardRepository implements CardRepository {
        ];
         return await pool.query(query, values);
     }
+
+    async getById(id: string): Promise<Card | null> {
+        const query = 'SELECT * FROM cards WHERE id = $1';
+        const result = await pool.query(query, [id]);
+
+        if (result.rows.length === 0) return null;
+
+        const row = result.rows[0];
+
+        return {
+            id: row.id,
+            question: row.question,
+            answer: row.answer,
+            interval: row.interval,
+            repetition: row.repetition,
+            easinessFactor: row.easiness_factor,
+            totalCardReviews: row.total_reviews,
+            dateAdded: row.date_added,
+            dateNextReview: row.date_next_review
+
+        }
+    }
+
+    async getDueCards(date: Date): Promise<Card[]> {
+        const query = 'SELECT * FROM cards WHERE date_next_review <= $1';
+
+        const result = await pool.query(query, [date]);
+
+        const rows: Card[] = [];
+        for (const row of result.rows) {
+            rows.push({
+                id: row.id,
+                question: row.question,
+                answer: row.answer,
+                interval: row.interval,
+                repetition: row.repetition,
+                easinessFactor: row.easiness_factor,
+                totalCardReviews: row.total_reviews,
+                dateAdded: row.date_added,
+                dateNextReview: row.date_next_review
+            });
+        }
+        return rows;
+    }
+
+    async updateCard(card: Card): Promise<void> {
+        const query = 'UPDATE cards SET question = $1, answer = $2, interval = $3, repetition = $4, easiness_factor = $5, date_next_review = $6 WHERE id = $7'
+
+        const values = [
+            card.question, // $1
+            card.answer, // $2
+            card.interval, // $3
+            card.repetition, // $4
+            card.easinessFactor, // $5
+            card.dateNextReview, // $6
+            card.id, // $7
+        ]
+
+        return await pool.query(query, values);
+    }
+
+    async deleteCard(id: string): Promise<void> {
+        const query = 'DELETE FROM cards WHERE id = $1';
+
+        return await pool.query(query, [id]);
+    }
 }
